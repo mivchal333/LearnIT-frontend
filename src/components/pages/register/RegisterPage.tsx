aa1e5e210e278b4a35c95db2b4e7992411ff862b
mivchal <mivchal@gmail.com>
2021-11-13 02:18:11 +0100

    registration page improvements

--- src/components/pages/register/RegisterPage.tsx
+++ src/components/pages/register/RegisterPage.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import RegisterForm from "./RegisterForm";
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles, Theme} from "@material-ui/core/styles";
 import {Grid, Paper} from "@material-ui/core";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {getStaticImageUrl, GIRL_LEARNING_LAPTOP_IMAGE_PATH} from "../../../service/staticProvider";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) => ({
+
+interface StyleProps {
+    imagePath: string,
+}
+
+const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
     root: {
         height: '100vh',
     },
     image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundImage: (props) => `url(${props.imagePath})`,
         backgroundRepeat: 'no-repeat',
         backgroundColor:
             theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
@@ -33,17 +40,33 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    titleWrapper: {
+        padding: theme.spacing(6, 2, 2, 2),
+        display: "flex",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: '2.4rem',
+        fontWeight: 'bold',
+    }
 }));
 
 
 const RegisterPage = () => {
-    const classes = useStyles();
-
+    const imagePath = getStaticImageUrl(GIRL_LEARNING_LAPTOP_IMAGE_PATH)
+    const classes = useStyles({imagePath});
 
     return (
         <Grid container component="main" className={classes.root}>
             <CssBaseline/>
-            <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+            <Grid item xs={false} sm={4} md={7} className={classes.image}>
+                <div className={classes.titleWrapper}>
+                    <Typography variant="h4" color="textPrimary" className={classes.title}>
+                        Wyeliminuj zgadywanie w trakcie rozwiązywania zadań domowych. Zarejestruj się już
+                        dziś.
+                    </Typography>
+                </div>
+            </Grid>
             <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
                 <RegisterForm/>
             </Grid>
--- src/service/staticProvider.ts
+++ src/service/staticProvider.ts
@@ -1,6 +1,7 @@
 export const EMPTY_IMAGE_PATH = 'empty-img.jpg'
 export const LEARNING_BANNER_IMAGE_PATH = 'learning-banner.webp'
 export const LEARNING_PROGRESS_IMAGE_PATH = 'learning-progress.webp'
+export const GIRL_LEARNING_LAPTOP_IMAGE_PATH = 'girl-learning-laptop.jpg'
 
 export const getStaticImageUrl = (imagePath: string) => {
     return process.env.PUBLIC_URL + '/img/' + imagePath
