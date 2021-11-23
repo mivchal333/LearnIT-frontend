3e5ece4682d0785296e31176c65fb4d11225fc7a
mivchal <mivchal@gmail.com>
2021-11-23 22:11:01 +0100

    delete question works

--- src/components/pages/technologies/listQuestions/QuestionList.tsx
+++ src/components/pages/technologies/listQuestions/QuestionList.tsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import {List, Paper} from "@material-ui/core";
 import QuestionListItem from "./QuestionListItem";
-import {useSelector} from "../../../../store/store";
+import {useDispatch, useSelector} from "../../../../store/store";
 import {selectTechnologyContextId} from "../../../../store/technologies/technologies.slice";
 import QuestionRepository from "../../../../api/repository/questions.repository";
 import {QuestionPreview} from "../../../../api/model/questionPreview.model";
 import {isNil} from "lodash-es";
 import {makeStyles} from "@material-ui/core/styles";
+import {useHistory} from "react-router-dom";
+import {GET_ROUTE} from "../../../../route/routes";
+import {deleteQuestion} from "../../../../store/technologies/actions";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -17,25 +20,41 @@ const useStyles = makeStyles((theme) => ({
 const QuestionList = () => {
     const [questions, setQuestions] = useState<QuestionPreview[]>([])
     const technologyId = useSelector(selectTechnologyContextId) || 0
+    const history = useHistory();
+    const dispatch = useDispatch()
+
     const classes = useStyles();
 
-    useEffect(() => {
-        const loadQuestions = async () => {
-            const {data} = await QuestionRepository.getQuestionsByTechnology(technologyId);
-            setQuestions(data)
-        }
+    const loadQuestions = async () => {
+        const {data} = await QuestionRepository.getQuestionsByTechnology(technologyId);
+        setQuestions(data)
+    }
 
+    useEffect(() => {
         if (!isNil(technologyId)) {
             loadQuestions()
         }
     }, [technologyId])
 
+    const onEdit = (questionId: number) => {
+        history.push(GET_ROUTE.TECHNOLOGY_QUESTION_EDIT(technologyId, questionId))
+    }
+
+    const onDelete = async (questionId: number) => {
+        await dispatch(deleteQuestion(questionId))
+        loadQuestions()
+    }
 
     return (
         <Paper className={classes.paper}>
             <List>
                 {questions.map(question => (
-                    <QuestionListItem question={question} key={question.id}/>
+                    <QuestionListItem
+                        question={question}
+                        key={question.id}
+                        onDelete={() => onDelete(question.id)}
+                        onEdit={() => onEdit(question.id)}
+                    />
                 ))}
             </List>
         </Paper>
--- src/components/pages/technologies/listQuestions/QuestionListItem.tsx
+++ src/components/pages/technologies/listQuestions/QuestionListItem.tsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import {QuestionPreview} from "../../../../api/model/questionPreview.model";
-import {Button, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import {Button, ButtonGroup, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import {useHistory} from "react-router-dom";
-import {GET_ROUTE} from "../../../../route/routes";
-import {usePathTechnologyContext} from "../../../../hooks/usePathTechnologyContext";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 interface PropsType {
     question: QuestionPreview,
+    onEdit: () => void,
+    onDelete: () => void,
 }
 
 const QuestionListItem = (props: PropsType) => {
-    const history = useHistory();
     const {question} = props
-
-    const [, technologyId] = usePathTechnologyContext();
-
-    const onClick = (questionId: number) => {
-        history.push(GET_ROUTE.TECHNOLOGY_QUESTION_EDIT(technologyId, questionId))
-    }
 
     return (
         <ListItem>
@@ -27,9 +20,14 @@ const QuestionListItem = (props: PropsType) => {
                 <HelpOutlineIcon/>
             </ListItemIcon>
             <ListItemText primary={question.body}/>
-            <Button startIcon={<EditIcon/>} variant="outlined" onClick={() => onClick(question.id)}>
-                Edytuj
-            </Button>
+            <ButtonGroup>
+                <Button startIcon={<EditIcon/>} onClick={props.onEdit}>
+                    Edytuj
+                </Button>
+                <Button startIcon={<DeleteIcon/>} onClick={props.onDelete}>
+                    Usuń
+                </Button>
+            </ButtonGroup>
         </ListItem>
     )
 }
