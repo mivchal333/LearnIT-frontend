762b387e2db8baeb0b8eec868db9714c2fcf5a02
mivchal <mivchal@gmail.com>
2022-01-30 12:51:12 +0100

    two class components refactor

--- src/components/pages/account/RolesField.tsx
+++ src/components/pages/account/RolesField.tsx
@@ -7,34 +7,37 @@ interface PropsType {
     roles: Role[],
 }
 
-const RolesField = (props: PropsType) => {
-    const {roles} = props;
-    return (
-        <Grid container spacing={1}>
-            <Grid item>
-                {includes(roles, Role.ADMIN) && (
-                    <Chip
-                        label={RoleLabel[Role.ADMIN]}
-                        color="secondary"
-                    />
-                )}
+class RolesField extends React.Component<PropsType> {
+    render() {
+
+        const {roles} = this.props;
+        return (
+            <Grid container spacing={1}>
+                <Grid item>
+                    {includes(roles, Role.ADMIN) && (
+                        <Chip
+                            label={RoleLabel[Role.ADMIN]}
+                            color="secondary"
+                        />
+                    )}
+                </Grid>
+                <Grid item>
+                    {includes(roles, Role.MOD) && (
+                        <Chip
+                            label={RoleLabel[Role.MOD]}
+                            color="primary"
+                        />
+                    )}
+                </Grid>
+                <Grid item>
+                    {includes(roles, Role.USER) && (
+                        <Chip
+                            label={RoleLabel[Role.USER]}
+                        />
+                    )}
+                </Grid>
             </Grid>
-            <Grid item>
-                {includes(roles, Role.MOD) && (
-                    <Chip
-                        label={RoleLabel[Role.MOD]}
-                        color="primary"
-                    />
-                )}
-            </Grid>
-            <Grid item>
-                {includes(roles, Role.USER) && (
-                    <Chip
-                        label={RoleLabel[Role.USER]}
-                    />
-                )}
-            </Grid>
-        </Grid>
-    )
+        )
+    }
 }
 export default RolesField
